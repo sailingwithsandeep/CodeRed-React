4b6b3b56f8662c6fd3def33e02b1c407ce42b4a0
SANDEEP-PARMAR-RKU <sparmar586@rku.ac.in>
2021-01-06 18:22:22 +0530

    first commit

--- src/ReactMenu/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = (props) => {
-  return <h1> Hello, I am a {props.name} Page </h1>;
-};
-
-export default About;
--- src/ReactMenu/Error.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Error = () => {
-  return <h1> OOps! Page not found! </h1>;
-};
-
-export default Error;
