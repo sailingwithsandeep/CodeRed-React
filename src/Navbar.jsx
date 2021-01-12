b43f9e927b4ceab88a086542507f33aee752e562
Dev Vora <52440769+DevVora626@users.noreply.github.com>
2021-01-12 19:52:32 +0530

    margin and padding added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,77 +2,77 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-	return (
-		<>
-			<div className='container-fluid nav_bg'>
-				<div className='row'>
-					<div className='col-10 mx-auto'>
-						<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-							<div className='container-fluid'>
-								<NavLink exact className='navbar-brand' to='/'>
-									CodeRed Technology
-								</NavLink>
-								<button
-									className='navbar-toggler'
-									type='button'
-									data-bs-toggle='collapse'
-									data-bs-target='#navbarSupportedContent'
-									aria-controls='navbarSupportedContent'
-									aria-expanded='false'
-									aria-label='Toggle navigation'
-								>
-									<span className='navbar-toggler-icon'></span>
-								</button>
-								<div
-									className='collapse navbar-collapse'
-									id='navbarSupportedContent'
-								>
-									<ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-										<li className='nav-item'>
-											<NavLink
-												activeClassName='menu_active'
-												className='nav-link'
-												to='/service'
-											>
-												Services
-											</NavLink>
-										</li>{" "}
-										<li className='nav-item'>
-											<NavLink
-												activeClassName='menu_active'
-												className='nav-link'
-												to='/team'
-											>
-												Team
-											</NavLink>
-										</li>{" "}
-										<li className='nav-item'>
-											<NavLink
-												activeClassName='menu_active'
-												className='nav-link'
-												to='/about'
-											>
-												About
-											</NavLink>
-										</li>
-										<li className='nav-item'>
-											<NavLink
-												activeClassName='menu_active'
-												className='nav-link'
-												to='/contact'
-											>
-												Contact Us
-											</NavLink>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container-fluid nav_bg mt-2">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                <NavLink exact className="navbar-brand" to="/">
+                  CodeRed Technology
+                </NavLink>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/service"
+                      >
+                        Services
+                      </NavLink>
+                    </li>{" "}
+                    <li className="nav-item">
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/team"
+                      >
+                        Team
+                      </NavLink>
+                    </li>{" "}
+                    <li className="nav-item">
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/about"
+                      >
+                        About
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        activeClassName="menu_active"
+                        className="nav-link"
+                        to="/contact"
+                      >
+                        Contact Us
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- src/Team.jsx
+++ src/Team.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Team = () => {
-	return (
-		<>
-			<h1>Welcome Team</h1>
-		</>
-	);
+  return (
+    <>
+      <h1 className="container-fluid my-5 col-10">Welcome Team</h1>
+    </>
+  );
 };
 export default Team;
