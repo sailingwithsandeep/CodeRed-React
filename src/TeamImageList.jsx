9c5eb0ee5ffb45d49eeb5f3b8547e88e3dfefb22
Dev Vora <52440769+DevVora626@users.noreply.github.com>
2021-01-17 18:49:50 +0530

    team component added

---
+++ src/TeamImageList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Assets/css/TeamImageList.css";
+import TeamMemberData from "./TeamMemberData";
+class TeamImageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+    this.state = {
+      imageUrlArray: TeamMemberData,
+    };
+  }
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpan);
+  }
+  setSpan = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+  render() {
+    let imageUrlArray = this.state.imageUrlArray;
+    const images = imageUrlArray.map(({ name, url }, index) => {
+      return (
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+          <img ref={this.imageRef} src={url} alt={name} key={index}></img>
+          <h4 className="text-center mt-1">{name}</h4>
+        </div>
+      );
+    });
+    return <div className="image-list">{images}</div>;
+  }
+}
+
+export default TeamImageList;
---
+++ src/TeamMemberData.jsx
@@ -0,0 +1,59 @@
+import ajay from "./Assets/Team/ProfileImages/ajaymarkana.jpg";
+import akshit from "./Assets/Team/ProfileImages/akshit.jpg";
+import deep from "./Assets/Team/ProfileImages/deepramani.jpg";
+import dev from "./Assets/Team/ProfileImages/devvora.jpg";
+import jeel from "./Assets/Team/ProfileImages/jeel.jpg";
+import krupali from "./Assets/Team/ProfileImages/krupali.jpg";
+import mit from "./Assets/Team/ProfileImages/mitpatel.jpg";
+import sandeep from "./Assets/Team/ProfileImages/sandeepparmar.jpg";
+import sanjana from "./Assets/Team/ProfileImages/sanjana.jpg";
+import vasu from "./Assets/Team/ProfileImages/vasutilva.jpg";
+import vinayak from "./Assets/Team/ProfileImages/vinayak.jpg";
+
+const data = [
+  {
+    url: ajay,
+    name: "Ajay Markna",
+  },
+  {
+    url: akshit,
+    name: "Akshit Zatakiya",
+  },
+  {
+    url: deep,
+    name: "Deep Ramani",
+  },
+  {
+    url: dev,
+    name: "Dev Vora",
+  },
+  {
+    url: jeel,
+    name: "Jeel Vanpariya",
+  },
+  {
+    url: krupali,
+    name: "Krupali Mehta",
+  },
+  {
+    url: mit,
+    name: "Mit Patel",
+  },
+  {
+    url: sandeep,
+    name: "Sandeep Parmar",
+  },
+  {
+    url: sanjana,
+    name: "Sanjana Purohit",
+  },
+  {
+    url: vasu,
+    name: "Vasu Tilva",
+  },
+  {
+    url: vinayak,
+    name: "Vinayak Varsani",
+  },
+];
+export default data;
